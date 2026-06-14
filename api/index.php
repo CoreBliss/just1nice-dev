c05535e20af34402eaf82379a5933214e481a24f
Justin Nice <justinviar200@gmail.com>
2026-06-15 03:22:51 +0700

    Vercel Commit

--- api/index.php
+++ api/index.php
@@ -1,15 +1,17 @@
 <?php
 
-$_SERVER['HTTP_ACCEPT'] = $_SERVER['HTTP_ACCEPT'] ?? 'application/json';
+$_SERVER['HTTP_ACCEPT'] = 'application/json';
 
 $_ENV['APP_BASE_PATH'] = dirname(__DIR__);
 $_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+
+putenv('APP_BASE_PATH=' . dirname(__DIR__));
 putenv('VIEW_COMPILED_PATH=/tmp/views');
 
 if (! is_dir('/tmp/views')) {
     mkdir('/tmp/views', 0755, true);
 }
 
-chdir(__DIR__ . '/..');
+chdir(dirname(__DIR__));
 
-require __DIR__ . '/../public/index.php';
+require dirname(__DIR__) . '/public/index.php';
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,20 +1,15 @@
 <?php
 
 use App\Http\Middleware\EnsureRole;
-use Illuminate\Auth\Access\AuthorizationException;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
-use Illuminate\Validation\ValidationException;
-use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Throwable;
+use Illuminate\View\ViewServiceProvider;
 
 return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        ViewServiceProvider::class,
+    ])
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
         api: __DIR__ . '/../routes/api.php',
@@ -26,4 +21,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'role' => EnsureRole::class,
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {})->create();
+    ->withExceptions(function (Exceptions $exceptions): void {
+        //
+    })
+    ->create();
