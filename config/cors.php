9adda44f024e17110b8fe96ff4c54beefa1210ba
Justin Nice <justinviar200@gmail.com>
2026-06-15 00:29:28 +0700

    Commit untuk  Deployment

--- config/cors.php
+++ config/cors.php
@@ -6,20 +6,6 @@ $frontendUrls = array_values(array_filter(array_map(
 )));
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => [
         'api/*',
         'sanctum/csrf-cookie',
@@ -36,7 +22,9 @@ return [
 
     'allowed_origins' => $frontendUrls,
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://.*\.vercel\.app$#',
+    ],
 
     'allowed_headers' => [
         'Accept',
